daab5a832a62999de8b08aebecca30c9d0f57c5c
W83220719 <1483795887@qq.com>
2020-01-07 19:09:18 +0800

    增加readrecordmapper

--- src/test/java/com/cheng/taskmanager/mapper/ReadRecordMapperTest.java
+++ src/test/java/com/cheng/taskmanager/mapper/ReadRecordMapperTest.java
@@ -23,15 +23,17 @@ public class ReadRecordMapperTest {
     @Autowired
     ReadRecordMapper mapper;
 
-    Date beginDate, endDate, outDate;
+    private Date beginDate, endDate, outDate, middleDate1, middleDate2;
 
-    ReadRecord readRecord;
+    private ReadRecord readRecord;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         beginDate = DateFactory.getDateFromString("2019-01-07");
         endDate = DateFactory.getDateFromString("2020-01-07");
-        outDate = DateFactory.getDateFromString("2019-01-06");
+        outDate = DateFactory.getDateFromString("2019-01-05");
+        middleDate1 = DateFactory.getDateFromString("2019-01-06");
+        middleDate2 = DateFactory.getDateFromString("2019-01-08");
 
         readRecord = new ReadRecord();
         readRecord.setDate(beginDate);
@@ -56,4 +58,47 @@ public class ReadRecordMapperTest {
         assertEquals(readRecord1.getDate().toString(), readRecord.getDate().toString());
         assertEquals(readRecord1.getRecord(), readRecord.getRecord());
     }
+
+    @Test
+    @Transactional
+    public void shouldRecordBeAddedUpWhenAddTwoRecordSameDay() {
+        int record1 = 10, record2 = 20;
+        readRecord.setRecord(record1);
+        mapper.addReadRecord(readRecord);
+        readRecord.setRecord(record2);
+        mapper.addReadRecord(readRecord);
+
+        List<ReadRecord> records = mapper.getReadRecords(beginDate, beginDate);
+        assertEquals(1, records.size());
+
+        ReadRecord readRecord1 = records.get(0);
+
+        assertEquals(record1 + record2, readRecord1.getRecord());
+    }
+
+    private void addRecord(Date date) {
+        readRecord.setDate(date);
+        mapper.addReadRecord(readRecord);
+    }
+
+    private void addTestRecords() {
+        addRecord(outDate);
+        addRecord(beginDate);
+        addRecord(endDate);
+    }
+
+    @Test
+    @Transactional
+    public void shouldGetSizeRightWhenGetRecordsAtBottomLine() {
+        addTestRecords();
+        assertEquals(2, mapper.getReadRecords(outDate, middleDate2).size());
+    }
+
+    @Test
+    @Transactional
+    public void shouldGetSizeRightWhenGetRecordsAtUpperLine() {
+        addTestRecords();
+        assertEquals(2, mapper.getReadRecords(middleDate1, endDate).size());
+    }
+
 }
